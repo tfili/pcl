60a96d4cfd3bb7cb96d946f5aa9c2ca34758bf77
Tom Fili <tfili@agi.com>
2014-11-07 10:28:29 -0500

    Fixed copy header script again.

--- CopyHeaders.sh
+++ CopyHeaders.sh
@@ -3,12 +3,14 @@
 if [[ -d "pcl/" ]]; then
     echo "Removing old pcl directory"
     `rm -rf ./pcl`
+    `mkdir ./pcl`
 fi
 
 printf "Copying includes."
+`cp include/pcl/* ./pcl/`
 for f in * ; do
     if [[ -d "$f/include/pcl/" && ! -L "$f/include/pcl/" ]]; then
-        `cp -R $f/include/pcl/* ./pcl`
+        `cp -R $f/include/pcl/* ./pcl/`
         printf "."
     fi
 done
